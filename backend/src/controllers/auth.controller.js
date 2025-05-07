08acbe546c8ac955bd9f3cba9facaac91a852b89
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-07 17:08:17 +0530

    Create auth.controller.js

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+
+export const signupController = (req, res) => {
+    // Add logic for signup here
+    res.send("Signup route logic will go here");
+  };
+  
+  export const loginController = (req, res) => {
+    // Add logic for login here
+    res.send("Login route logic will go here");
+  };
+  
+  export const logoutController = (req, res) => {
+    // Add logic for logout here
+    res.send("Logout route logic will go here");
+  };
+  
