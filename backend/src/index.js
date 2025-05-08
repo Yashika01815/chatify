299ed01584f5659255c33f2f9e2dbe7e619a3217
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-08 17:31:19 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import messageRoutes from './routes/message.route.js';
+import cors from 'cors';
 import {connectDB} from './lib/db.js';
 dotenv.config();
 const app = express();
@@ -8,8 +11,9 @@ const PORT = process.env.PORT
 
 
 app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cookieParser()); // Middleware to parse cookies
 app.use("/api/auth", authRoutes);
-
+app.use("/api/messages", messageRoutes);
 
 
 
