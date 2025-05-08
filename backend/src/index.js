5bd732a142c2b3f6a2546efb18d81e303f16ccef
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-08 12:41:18 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
-
+import {connectDB} from './lib/db.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT
 
 
+app.use(express.json()); // Middleware to parse JSON request bodies
 app.use("/api/auth", authRoutes);
 
 
@@ -21,4 +22,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on port:' + PORT);
+    connectDB()
     });
