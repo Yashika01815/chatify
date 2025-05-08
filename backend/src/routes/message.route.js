779c84f5c0c53cfaffe189daf6d265a1eaf47b1d
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-08 18:08:47 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,12 @@
-import express from 'express'
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
-const Router= express.Router();
+const router = express.Router();
 
-export default Router;
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
