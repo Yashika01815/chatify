f88f40651aba9b181db0ca9de99f8df221e70064
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-08 17:31:23 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js'; // Adjust the path based on your project structure
 
-import { signupController, loginController, logoutController, updateProfileController  } from '../controllers/auth.controller.js';
+import { checkAuth, signupController, loginController, logoutController, updateProfileController  } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.post("/login", loginController);
 router.post("/logout", logoutController);
 
 router.put("/update-profile",protectRoute, updateProfileController);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
