306dcc97e191860f33792ec28e3aac0d7be959e8
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-11 18:33:53 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,16 +4,21 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
+import path from "path";
+
 import {connectDB} from './lib/db.js';
+import { app, server } from "./lib/socket.js";
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT
+const __dirname = path.resolve();
+
 
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(cookieParser()); // Middleware to parse cookies
 app.use(cors({
-    origin: "http://localhost:5001", // Allow requests from this origin (frontend)
+    origin: "http://localhost:5173", // Allow requests from this origin (frontend)
     credentials: true, // Allow credentials (cookies) to be sent
 })); // Middleware to enable CORS
 app.use("/api/auth", authRoutes);
@@ -21,7 +26,13 @@ app.use("/api/messages", messageRoutes);
 
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 
