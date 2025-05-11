9b46055105187290416b0b47051f68c28c777a67
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-11 18:32:13 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
+
+
+//this file is making a connection to the backend server using axios and setting the base url to the backend server url
