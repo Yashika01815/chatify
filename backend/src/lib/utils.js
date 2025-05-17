788847c63d7762b7c8edb3650e4ad71ffc18fc6c
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-17 18:54:14 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET, {
     expiresIn: '7d',
   });
 
