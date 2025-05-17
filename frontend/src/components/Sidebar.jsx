660b3e1b7c52b8e7393feedef8d926979ec0d138
Yashika thakkar <yashika.thakkar.167417@gmail.com>
2025-05-17 18:55:01 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
-          <Users className="size-6" />
+          <Users size={24} />
           <span className="font-medium hidden lg:block">Contacts</span>
         </div>
         {/* TODO: Online filter toggle */}
@@ -57,7 +57,7 @@ const Sidebar = () => {
               <img
                 src={user.profilePic || "/avatar.png"}
                 alt={user.name}
-                className="size-12 object-cover rounded-full"
+                className="w-9 h-9 object-cover rounded-full"
               />
               {onlineUsers.includes(user._id) && (
                 <span
